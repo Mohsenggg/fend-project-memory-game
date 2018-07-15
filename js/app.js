2970100b698cdee19455c0aaa3bd49edea610698
Mohsenggg <36776695+Mohsenggg@users.noreply.github.com>
2018-07-15 11:04:23 +0200

    Compare between two cards

--- js/app.js
+++ js/app.js
@@ -1,14 +1,13 @@
-
- //  Create a list that holds all of your cards
-const items = ['fa-diamond', 'fa-diamond'
-                'fa-paper-plane-o', 'fa-paper-plane-o'
-                'fa-anchor', 'fa-anchor'
-                'fa-bolt', 'fa-bolt'
-                'fa-cube', 'fa-cube'
-                'fa-leaf', 'fa-leaf'
-                'fa-bicycle', 'fa-bicycle'
-                'fa-bomb', 'fa-bomb'
-                ];
+//  Create a list that holds all of your cards
+const items = ["fa fa-diamond", "fa fa-diamond",
+               "fa fa-paper-plane-o","fa fa-paper-plane-o",
+               "fa fa-anchor", "fa fa-anchor",
+               "fa fa-bolt", "fa fa-bolt",
+               "fa fa-cube", "fa fa-cube",
+               "fa fa-leaf", "fa fa-leaf",
+               "fa fa-bicycle", "fa fa-bicycle",
+               "fa fa-bomb", "fa fa-bomb",
+               ];
 
 /* Display the cards on the page
 *   - shuffle the list of cards using the provided "shuffle" method below
@@ -17,101 +16,111 @@ const items = ['fa-diamond', 'fa-diamond'
 */
 // Shuffle function from http://stackoverflow.com/a/2450976
 function shuffle(array) {
-    var currentIndex = array.length, temporaryValue, randomIndex;
+   var currentIndex = array.length, temporaryValue, randomIndex;
 
-    while (currentIndex !== 0) {
-        randomIndex = Math.floor(Math.random() * currentIndex);
-        currentIndex -= 1;
-        temporaryValue = array[currentIndex];
-        array[currentIndex] = array[randomIndex];
-        array[randomIndex] = temporaryValue;
-    }
+   while (currentIndex !== 0) {
+       randomIndex = Math.floor(Math.random() * currentIndex);
+       currentIndex -= 1;
+       temporaryValue = array[currentIndex];
+       array[currentIndex] = array[randomIndex];
+       array[randomIndex] = temporaryValue;
+   }
 
-    return array;
+   return array;
 }
 
 /*
- * set up the event listener for a card. If a card is clicked:
- *  - display the card's symbol (put this functionality in another function that you call from this one)
- *  - add the card to a *list* of "open" cards (put this functionality in another function that you call from this one)
- *  - if the list already has another card, check to see if the two cards match
- *    + if the cards do match, lock the cards in the open position (put this functionality in another function that you call from this one)
- *    + if the cards do not match, remove the cards from the list and hide the card's symbol (put this functionality in another function that you call from this one)
- *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
- *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
- */
+* set up the event listener for a card. If a card is clicked:
+*  - display the card's symbol (put this functionality in another function that you call from this one)
+*  - add the card to a *list* of "open" cards (put this functionality in another function that you call from this one)
+*  - if the list already has another card, check to see if the two cards match
+*    + if the cards do match, lock the cards in the open position (put this functionality in another function that you call from this one)
+*    + if the cards do not match, remove the cards from the list and hide the card's symbol (put this functionality in another function that you call from this one)
+*    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
+*    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
+*/
 
- const deck = document.querySelector('.deck');
- let mins = document.getElementById('mins');
- let sec = document.getElementById('sec');
- const moveNumber = document.querySelector('.moves');
- let moves;
- let openedCards = [];
- let matchedCards = [];
-const previousCard = openedCards[0];
+const deck = document.querySelector('.deck');
+let mins = document.getElementById('mins');
+let sec = document.getElementById('sec');
+const moveNumber = document.querySelector('.moves');
+let moves;
+let openedCards = [];
+let matchedCards = [];
+let isFirstClick = true;
+
 // Start the game
 function start() {
-  // shuffle the cards
-  const shuffledCard = shuffle(items);
-  //add cardS
-  for (var i = 0; i < items.length; i++) {
-    const card = document.createElement("li");
-    card.classList.add('card');
-    card.innerHtml = '<i class='${items[i]}'></i>';
-    deck.appendChild(card);
+ // shuffle the cards
+ const shuffledCard = shuffle(items);
+ //add cardS
+ for (let i = 0; i < items.length; i++) {
+   const card = document.createElement("li");
+   card.classList.add('card');
+   card.innerHTML = `<i class="${items[i]}"></i>`;
+   deck.appendChild(card);
 
-    card.addEventListener('click', appear)
-    // when start moves = 0
-    moves = 0;
-    moveNumber.textContent = moves;
-  }
+   // when start moves = 0
+   moves = 0;
+   moveNumber.textContent = moves;
+
+   appear(card);
+ }
 }
 
 // Add event listner for click and show card
-function appear(){
-  //OPEN first card or second card & compare between them
-  const viewCard = this;
+function appear(card){
+ //OPEN first card or second card & compare between them
+ card.addEventListener('click', function () {
 
-         if (openedCards.length === 1) {
-           card.classList.add("open", "show");
-           openedCards.push(this);
-           compare(viewCard, previousCard);
 
-        } else {
+      const viewCard = this;
+      const previousCard = openedCards[0];
+
+        if (openedCards.length === 1) {
           card.classList.add("open", "show");
           openedCards.push(this);
-        }
+          compare(viewCard, previousCard);
+       } else {
+         card.classList.add("open", "show");
+         openedCards.push(this);
+       }
 
-  //match timer start with first click
-      if (firstClick === true) {
-        timer();
-        firstClick = false;
-      }
+       //match timer start with first click
+       if (isFirstClick === true) {
+         timer();
+         isFirstClick = false;
+       }
+ });
 }
-
 // compare between two oppened card
 function compare(viewCard, previousCard) {
-          // when 2 cards matched
-        if(viewCard.innerHtml === previousCard.innerHtml){
-          viewCard.classList.add('match');
-          previousCard.classList.add('match');
-          matchedCards.push(viewCard, previousCard);
-          openedCards = [];
-          // when 2 cards not matched
-        } eles {
-          viewCard.classList.remove('open', 'show');
-          previousCard.classList.remove('open', 'show');
-          openedCards = [];
-        }
+         // when 2 cards matched
+       if(viewCard.innerHTML === previousCard.innerHTML){
+         viewCard.classList.add('match');
+         previousCard.classList.add('match');
+         matchedCards.push(viewCard, previousCard);
+         openedCards = [];
+         // when 2 cards not matched
+       } else {
+         // show the second card for a 900s
+         setTimeout(function () {
+           viewCard.classList.remove('open', 'show');
+           previousCard.classList.remove('open', 'show');
+         }, 900);   
+         openedCards = [];
+       }
 }
 
 // set timer for the game
 function timer() {
-  setInterval(function(){
-    sec.innerText++;
-    if (sec.innerText == 60) {
-      mins.innerText++;
-      sec.innerText = 0;
-    }
-  },1000);
+ setInterval(function(){
+   sec.innerText++;
+   if (sec.innerText == 60) {
+     mins.innerText++;
+     sec.innerText = 0;
+   }
+ },1000);
 }
+
+start();
